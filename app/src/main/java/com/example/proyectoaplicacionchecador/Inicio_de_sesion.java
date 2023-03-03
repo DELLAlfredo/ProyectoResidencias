c1941f413d6b489a51f8ec3cb43614ade68496f1
Noizel01Edel <125685718+Noizel01Edel@users.noreply.github.com>
2023-03-03 10:29:10 -0700

    Validacion de campo vacio de usuario SetOnClickListener de button IniciarSession

--- app/src/main/java/com/example/proyectoaplicacionchecador/Inicio_de_sesion.java
+++ app/src/main/java/com/example/proyectoaplicacionchecador/Inicio_de_sesion.java
@@ -3,14 +3,38 @@ package com.example.proyectoaplicacionchecador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Inicio_de_sesion extends AppCompatActivity {
+    Button btnInicarSesion;
+    EditText EdUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_de_sesion);
+
+    btnInicarSesion     = findViewById(R.id.btnInicarSesion);
+    EdUsuario           = findViewById(R.id.EdUsuario);
+
+
+
+    btnInicarSesion.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(!EdUsuario.getText().toString().isEmpty()){
+
+            }else{
+                camposVacios();
+            }
+        }
+    });
+
+
+
     }
 
     private void camposVacios(){
---
+++ app/src/main/java/com/example/proyectoaplicacionchecador/abcMaestros.java
@@ -0,0 +1,14 @@
+package com.example.proyectoaplicacionchecador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class abcMaestros extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_abc_maestros);
+    }
+}
