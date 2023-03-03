415b8438c3727e53602f3eae166ebfecb507fbe5
ImPan014 <luisfranciscojimenezmorales@gmail.com>
2023-03-03 07:48:49 -0700

    francisco tarea sprint 3

--- app/src/main/java/com/example/proyectoaplicacionchecador/Inicio_de_sesion.java
+++ app/src/main/java/com/example/proyectoaplicacionchecador/Inicio_de_sesion.java
@@ -3,6 +3,7 @@ package com.example.proyectoaplicacionchecador;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Inicio_de_sesion extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class Inicio_de_sesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_de_sesion);
+    }
+
+    private void camposVacios(){
+        Toast.makeText(Inicio_de_sesion.this, "Campos vacios", Toast.LENGTH_SHORT).show();
     }
 }
