4095bf63f49b72a7900ec15248aae356d926f2bd
DELLAlfredo <119427135+DELLAlfredo@users.noreply.github.com>
2023-03-04 09:06:14 -0700

    Update Inicio_de_sesion.java

--- app/src/main/java/com/example/proyectoaplicacionchecador/Inicio_de_sesion.java
+++ app/src/main/java/com/example/proyectoaplicacionchecador/Inicio_de_sesion.java
@@ -2,6 +2,8 @@ package com.example.proyectoaplicacionchecador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,8 +12,13 @@ import android.widget.Toast;
 
 public class Inicio_de_sesion extends AppCompatActivity {
     Button btnInicarSesion;
-    EditText EdUsuario;
+    EditText EdUsuario, EdContraseña;
+    private String password = "123";
+    private String user = "root";
 
+
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,15 +26,22 @@ public class Inicio_de_sesion extends AppCompatActivity {
 
     btnInicarSesion     = findViewById(R.id.btnInicarSesion);
     EdUsuario           = findViewById(R.id.EdUsuario);
-
+    EdContraseña           = findViewById(R.id.EdContraseña);
 
 
     btnInicarSesion.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if(!EdUsuario.getText().toString().isEmpty()){
+            if(!EdUsuario.getText().toString().equalsIgnoreCase(user)&&
+                    EdContraseña.getText().toString().equalsIgnoreCase(password)){
+                Toast.makeText(getApplicationContext(),"Ha iniciado sesión correctamente",Toast.LENGTH_SHORT).show();
+                //Intent intent = new Intent(this, OtraActividad.class);  //Creamos el intent y le indicamos desde donde vamos (this) y a donde vamos (OtraActividad.class)
+                //startActivity(intent);  //Abrimos la otra actividad
+
+
 
             }else{
+                Toast.makeText(getApplicationContext(),"User o contraseña incorrectos",Toast.LENGTH_SHORT).show();
                 camposVacios();
             }
         }
