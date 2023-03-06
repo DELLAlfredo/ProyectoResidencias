b57434a7c6081202bc6cc31c2f970306a2125b32
Cruzdelangelrubiofierro <125680938+Cruzdelangelrubiofierro@users.noreply.github.com>
2023-03-06 09:07:54 -0700

    boton de regresar

--- app/src/main/java/com/example/proyectoaplicacionchecador/abcAulas.java
+++ app/src/main/java/com/example/proyectoaplicacionchecador/abcAulas.java
@@ -2,14 +2,18 @@ package com.example.proyectoaplicacionchecador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
 public class abcAulas extends AppCompatActivity {
-    Button btnAceptar;
+    Button btnAceptar,btnregresar;
+
     EditText etnombre;
     int joto = 0;
     Spinner spAccion;
@@ -22,5 +26,15 @@ public class abcAulas extends AppCompatActivity {
         String[] lenguajes = {"Seleccione","Alta","Baja","Cambio"};
         ArrayAdapter<String> Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lenguajes);
         spAccion.setAdapter(Adapter);
+        btnregresar.findViewById(R.id.btnregresar);
+
+        btnregresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reg = new Intent(getApplicationContext(),Inicio_de_sesion.class);
+                startActivity(reg);
+            }
+        });
+
     }
 }
