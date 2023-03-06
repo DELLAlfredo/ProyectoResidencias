4e8155d7a3310f3e55837bc9e8a58cac2c958a44
Noizel01Edel <125685718+Noizel01Edel@users.noreply.github.com>
2023-03-06 09:34:46 -0700

    SetOnCLiclkListener, declaracion de variable btnAceptar

--- app/src/main/java/com/example/proyectoaplicacionchecador/abcAulas.java
+++ app/src/main/java/com/example/proyectoaplicacionchecador/abcAulas.java
@@ -2,7 +2,9 @@ package com.example.proyectoaplicacionchecador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,12 +18,25 @@ public class abcAulas extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        btnAceptar  =findViewById(R.id.btnAceptar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abc_aulas);
         spAccion = (Spinner)findViewById(R.id.spAccion);
         String[] lenguajes = {"Seleccione","Alta","Baja","Cambio"};
         ArrayAdapter<String> Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lenguajes);
         spAccion.setAdapter(Adapter);
+
+
+
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
     public void onCreate(SQLiteDatabase db) {
     }
