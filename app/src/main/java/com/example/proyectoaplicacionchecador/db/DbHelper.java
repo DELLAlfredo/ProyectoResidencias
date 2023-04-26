0a5a59eb524c50cb5fa34ef3950e1483ca172230
DELLAlfredo <119427135+DELLAlfredo@users.noreply.github.com>
2023-04-26 16:52:34 -0700

    CARRERA_Y_ACADEMIA

--- app/src/main/java/com/example/proyectoaplicacionchecador/db/DbHelper.java
+++ app/src/main/java/com/example/proyectoaplicacionchecador/db/DbHelper.java
@@ -8,35 +8,44 @@ import androidx.annotation.Nullable;
 
 public class DbHelper extends SQLiteOpenHelper {
 
-   /* private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 2;
     private static final String DATABASE_NOMBRE = "db_checador";
-    public static final String TABLE_MAESTROS = "t_maestros"; */
+    public static final String TABLE_MAESTROS = "t_maestros";
+    public static final String TABLE_CARRERA = "t_carrera";
+    public static final String TABLE_ACADEMIA = "t_academia";
 
 
 
-    public DbHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version/*@Nullable Context context*/) {
-      //  super(context, DATABASE_NOMBRE,null, DATABASE_VERSION);
-        super(context, name, factory, version);
+    public DbHelper(@Nullable Context context) {
+        super(context, DATABASE_NOMBRE,null, DATABASE_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db /*sqLiteDatabase*/) {
-       /* sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_MAESTROS + "("+
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_MAESTROS + "("+
                 "id_maestro INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "+
                 "nombre TEXT NOT NULL,"+
                 "apellidos TEXT NOT NULL,"+
-                "academia TEXT NOT NULL)");*/
-        db.execSQL("create table t_maestros (id int primary key, nombre TEXT, apellidos TEXT, academia TEXT)");
-        db.execSQL("create table t_clase (id int primary key, hora TEXT, fecha TEXT, academia TEXT, id_aula int, id_carrera int, id_maestro int)");
+                "academia TEXT NOT NULL)");
 
+        sqLiteDatabase.execSQL("create table carreras(id int primary key, nombre text)");
 
+        sqLiteDatabase.execSQL("create table carreras(id int primary key, codigo text)");
+
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_CARRERA + "("+
+                "id_carrera INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "+
+                "nombre TEXT NOT NULL)");
+
+        sqLiteDatabase.execSQL("CREATE TABLE " + TABLE_ACADEMIA + "("+
+                "id_academia INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "+
+                "codigo TEXT NOT NULL)");
     }
+
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion/*SQLiteDatabase sqLiteDatabase, int i, int i1*/) {
-      //  sqLiteDatabase.execSQL("DROP TABLE " + TABLE_MAESTROS);
-      //  onCreate(sqLiteDatabase);
-
-
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE " + TABLE_MAESTROS);
+        onCreate(sqLiteDatabase);
     }
+
 }
