18f3d3fd72b424fbe3e31b93bef877dc7e238fcc
Cruzdelangelrubiofierro <125680938+Cruzdelangelrubiofierro@users.noreply.github.com>
2023-05-05 20:42:00 -0700

    creacion de pantallas faltantes

--- app/src/main/java/com/example/proyectoaplicacionchecador/abcAulas.java
+++ app/src/main/java/com/example/proyectoaplicacionchecador/abcAulas.java
@@ -12,32 +12,15 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 
 public class abcAulas extends AppCompatActivity {
-    Button btnAceptar;
-
-
-    EditText etnombre;
-    int joto = 0;
-    Spinner spAccion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abc_aulas);
-        btnAceptar = findViewById(R.id.btnAceptar);
-        etnombre.findViewById(R.id.etNombre);
 
-        spAccion = (Spinner)findViewById(R.id.spAccion);
-        String[] lenguajes = {"Seleccione","Alta","Baja","Cambio"};
-        ArrayAdapter<String> Adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, lenguajes);
-        spAccion.setAdapter(Adapter);
 
-        btnAceptar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
 
 
 
