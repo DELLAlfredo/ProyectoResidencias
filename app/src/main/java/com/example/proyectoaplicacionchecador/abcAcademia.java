67a4f5ebed8b9fefd8f6621e42dcd8e86bfaad3d
DELLAlfredo <119427135+DELLAlfredo@users.noreply.github.com>
2023-05-12 21:26:35 -0700

    saa

--- app/src/main/java/com/example/proyectoaplicacionchecador/abcAcademia.java
+++ app/src/main/java/com/example/proyectoaplicacionchecador/abcAcademia.java
@@ -1,21 +1,182 @@
 package com.example.proyectoaplicacionchecador;
 
+import static android.graphics.Insets.add;
+
+import static androidx.core.content.SharedPreferencesKt.edit;
+
+import static java.nio.file.Files.delete;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.proyectoaplicacionchecador.db.DbHelper;
 
 public class abcAcademia extends AppCompatActivity {
+    EditText etIDacademia,etCodigo;
+    Spinner spinerAcademia;
+    Button btnAceptar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abc_academia);
+
+        etIDacademia = findViewById(R.id.etIDacademia);
+        etCodigo = findViewById(R.id.etCodigo);
+        spinerAcademia = findViewById(R.id.spinerAcademia);
+        btnAceptar = findViewById(R.id.btnAceptar);
+
+        String[] crud = {"Añadir","Actualizar","Buscar","Eliminar"};
+        ArrayAdapter<String> AdapterCrud = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, crud);
+        spinerAcademia.setAdapter(AdapterCrud);
+        
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch(spinerAcademia.getSelectedItem().toString()) {
+                    case "Añadir":
+                        if (!etIDacademia.getText().toString().isEmpty() &&
+                                !etCodigo.getText().toString().isEmpty()) {
+                            add(Integer.parseInt(etIDacademia.getText().toString()), etCodigo.getText().toString(),spinerAcademia.getSelectedItem().toString());
+                        }else{
+                            Toast.makeText(abcAcademia.this, "Debes llenar los demas campos", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case "Actualizar":
+                        if (!etIDacademia.getText().toString().isEmpty() &&
+                                !etCodigo.getText().toString().isEmpty()) {
+                            edit(Integer.parseInt(etIDacademia.getText().toString()), etCodigo.getText().toString(),spinerAcademia.getSelectedItem().toString());
+                        }else{
+                            Toast.makeText(abcAcademia.this, "Los campos estan vacios", Toast.LENGTH_SHORT).show();
+                        }
+                        break;
+                    case "Eliminar":
+                        if(!etIDacademia.getText().toString().isEmpty()){
+                            delete(Integer.parseInt(etIDacademia.getText().toString()), spinerAcademia.getSelectedItem().toString());
+                        }
+                        break;
+                    case "Buscar":
+                        if(!etIDacademia.getText().toString().isEmpty()){
+                            search(Integer.parseInt(etIDacademia.getText().toString()), spinerAcademia.getSelectedItem().toString());
+                        }
+                        break;
+
+                }
+
+            }
+                
+            
+        });
     }
+
+    private void search(int code, String spinerAcademia) {
+        if(spinerAcademia.equals("Buscar")) {
+            if (code != 0) {
+                DbHelper admin = new DbHelper(abcAcademia.this, "POS", null, 2);
+                SQLiteDatabase db = admin.getWritableDatabase();
+                Cursor cursor = db.rawQuery("select id_academia, nombre from t_academia where id_academia=" + code, null);
+                if (cursor.moveToFirst()) {
+                    etIDacademia.setText(cursor.getString(0));
+                    etCodigo.setText(cursor.getString(1));
+                    db.close();
+
+                } else {
+                    Toast.makeText(abcAcademia.this, "El codigo que desea buscar no existe! ", Toast.LENGTH_SHORT).show();
+
+                }
+            } else {
+                Toast.makeText(abcAcademia.this, "Escribe un codigo valido, no puede ser zero", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void delete(int code, String spinerAcademia) {
+        if (spinerAcademia.equals("Eliminar")) {
+            DbHelper admin = new DbHelper(abcAcademia.this, "POS", null, 2);
+            SQLiteDatabase db = admin.getWritableDatabase();
+            int result = db.delete("t_academia", "id_academia=" + code, null);
+
+            admin.close();
+            etIDacademia.setText("");
+            etCodigo.setText("");
+
+            if (result >= 1) {
+                Toast.makeText(abcAcademia.this, "Se elimino al usuario", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(abcAcademia.this, "El usuario que desea eliminar, no existe!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void edit(int code, String etCodigo, String spinerAcademia) {
+
+        if (spinerAcademia.equals("Actualizar")) {
+            DbHelper admin = new DbHelper(abcAcademia.this, "POS", null, 2);
+            SQLiteDatabase db = admin.getWritableDatabase();
+
+            ContentValues registry = new ContentValues();
+
+            registry.put("id_academia", code);
+            registry.put("codigo", etCodigo);
+
+
+            int result = db.update("t_academia", registry,"id_academia ="+code, null);
+            if (result != 0){
+                Toast.makeText(abcAcademia.this, "Registro actualizado", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(abcAcademia.this, "No se puedo actualizar el registro, verifique por favor", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void add(int code, String codigo, String spinerAcademia) {
+        if (spinerAcademia.equals("Añadir")) {
+
+            if (code != 0) {
+                DbHelper admin = new DbHelper(abcAcademia.this, "POS", null, 2);
+                SQLiteDatabase db = admin.getReadableDatabase();
+                Cursor cursor = db.rawQuery("SELECT id_academia,nombre from t_academia where id_academia ="+code, null);
+                if (cursor.moveToFirst()) {
+                    Toast.makeText(abcAcademia.this, "Codigo ya existente \n elija otro codigo", Toast.LENGTH_LONG).show();
+                } else {
+                    ContentValues registro = new ContentValues();
+                    registro.put("id_academia", code);
+                    registro.put("codigo", codigo);
+
+
+                    db.insert("t_academia", null, registro);
+                    db.close();
+                    etIDacademia.setText("");
+                    etCodigo.setText("");
+                    Toast.makeText(abcAcademia.this, "Se guardo el registro", Toast.LENGTH_SHORT).show();
+
+                }
+
+            } else {
+                Toast.makeText(abcAcademia.this, "No se pudo añadir, llena los campos", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+
+
+
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.opcions_menu, menu);
--- app/src/main/java/com/example/proyectoaplicacionchecador/abcAulas.java
+++ app/src/main/java/com/example/proyectoaplicacionchecador/abcAulas.java
@@ -1,9 +1,18 @@
 package com.example.proyectoaplicacionchecador;
 
+import static android.graphics.Insets.add;
+
+import static androidx.core.content.SharedPreferencesKt.edit;
+
+import static java.nio.file.Files.delete;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,6 +23,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.proyectoaplicacionchecador.db.DbHelper;
 
 public class abcAulas extends AppCompatActivity {
     EditText edNombreAula,edIDAula;
@@ -37,10 +49,134 @@ public class abcAulas extends AppCompatActivity {
         spinerAula.setAdapter(AdapterCrud);
 
 
+        btnAceptarAu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    switch(spinerAula.getSelectedItem().toString()) {
+                        case "Añadir":
+                            if (!edIDAula.getText().toString().isEmpty() &&
+                                    !edNombreAula.getText().toString().isEmpty()) {
+                                add(Integer.parseInt(edIDAula.getText().toString()), edNombreAula.getText().toString(),spinerAula.getSelectedItem().toString());
+                            }else{
+                                Toast.makeText(abcAulas.this, "Debes llenar los demas campos", Toast.LENGTH_SHORT).show();
+                            }
+                            break;
+                        case "Actualizar":
+                            if (!edIDAula.getText().toString().isEmpty() &&
+                                    !edNombreAula.getText().toString().isEmpty()) {
+                                edit(Integer.parseInt(edIDAula.getText().toString()), edNombreAula.getText().toString(),spinerAula.getSelectedItem().toString());
+                            }else{
+                                Toast.makeText(abcAulas.this, "Los campos estan vacios", Toast.LENGTH_SHORT).show();
+                            }
+                            break;
+                        case "Eliminar":
+                            if(!edIDAula.getText().toString().isEmpty()){
+                                delete(Integer.parseInt(edIDAula.getText().toString()), spinerAula.getSelectedItem().toString());
+                            }
+                            break;
+                        case "Buscar":
+                            if(!edIDAula.getText().toString().isEmpty()){
+                                search(Integer.parseInt(edIDAula.getText().toString()), spinerAula.getSelectedItem().toString());
+                            }
+                            break;
 
+                    }
+
+                }
+
+        });
 
 
     }
+
+    private void search(int code, String spinerAula) {
+        if(spinerAula.equals("Buscar")) {
+            if (code != 0) {
+                DbHelper admin = new DbHelper(abcAulas.this, "POS", null, 2);
+                SQLiteDatabase db = admin.getWritableDatabase();
+                Cursor cursor = db.rawQuery("select id_aula, nombre from t_aula where id_aula=" + code, null);
+                if (cursor.moveToFirst()) {
+                    edIDAula.setText(cursor.getString(0));
+                    edNombreAula.setText(cursor.getString(1));
+                    db.close();
+
+                } else {
+                    Toast.makeText(abcAulas.this, "El codigo que desea buscar no existe! ", Toast.LENGTH_SHORT).show();
+
+                }
+            } else {
+                Toast.makeText(abcAulas.this, "Escribe un codigo valido, no puede ser zero", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void delete(int code, String spinerAula) {
+        if (spinerAula.equals("Eliminar")) {
+            DbHelper admin = new DbHelper(abcAulas.this, "POS", null, 2);
+            SQLiteDatabase db = admin.getWritableDatabase();
+            int result = db.delete("t_aula", "id_aula=" + code, null);
+
+            admin.close();
+            edIDAula.setText("");
+            edNombreAula.setText("");
+
+            if (result >= 1) {
+                Toast.makeText(abcAulas.this, "Se elimino al usuario", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(abcAulas.this, "El usuario que desea eliminar, no existe!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void edit(int code, String nombre, String spinerAula) {
+        if (spinerAula.equals("Actualizar")) {
+            DbHelper admin = new DbHelper(abcAulas.this, "POS", null, 2);
+            SQLiteDatabase db = admin.getWritableDatabase();
+
+            ContentValues registry = new ContentValues();
+
+            registry.put("id_aula", code);
+            registry.put("nombre", nombre);
+
+
+            int result = db.update("t_aula", registry,"id_aula ="+code, null);
+            if (result != 0){
+                Toast.makeText(abcAulas.this, "Registro actualizado", Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(abcAulas.this, "No se puedo actualizar el registro, verifique por favor", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void add(int code, String nombre, String spinerAula) {
+        if (spinerAula.equals("Añadir")) {
+
+            if (code != 0) {
+                DbHelper admin = new DbHelper(abcAulas.this, "POS", null, 2);
+                SQLiteDatabase db = admin.getReadableDatabase();
+                Cursor cursor = db.rawQuery("SELECT id_aula,nombre from t_aula where id_aula ="+code, null);
+                if (cursor.moveToFirst()) {
+                    Toast.makeText(abcAulas.this, "Codigo ya existente \n elija otro codigo", Toast.LENGTH_LONG).show();
+                } else {
+                    ContentValues registro = new ContentValues();
+                    registro.put("id_aula", code);
+                    registro.put("nombre", nombre);
+
+
+                    db.insert("t_aula", null, registro);
+                    db.close();
+                    edIDAula.setText("");
+                    edNombreAula.setText("");
+                    Toast.makeText(abcAulas.this, "Se guardo el registro", Toast.LENGTH_SHORT).show();
+
+                }
+
+            } else {
+                Toast.makeText(abcAulas.this, "No se pudo añadir, llena los campos", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.opcions_menu, menu);
